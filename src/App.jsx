cfcfd24a991eba4372596c44403b60a89866add0
rohanprajapati <rohanprajapati369@gmail.com>
2024-02-07 17:34:09 +0545

    Remove unused code and update export statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import './App.css'
 import Quiz from './components/quiz'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -12,4 +9,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
